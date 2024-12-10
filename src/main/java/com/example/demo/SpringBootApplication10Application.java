aef94e0cbdbb12abc176b2a09c5f8e286b5ac7f5
trupti-deole <truptideole9@gmail.com>
2024-12-10 13:24:00 +0530

    y variable added

--- src/main/java/com/example/demo/SpringBootApplication10Application.java
+++ src/main/java/com/example/demo/SpringBootApplication10Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootApplication10Application {
-
+    int y=200;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication10Application.class, args);
 	}

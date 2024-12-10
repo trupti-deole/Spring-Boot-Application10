8db1623801ddb2ec1fe16d01cbffe61a82414200
saurabhtipule <tipulesaurabh12@gmail.com>
2024-12-10 13:29:13 +0530

    m1() method added

--- src/main/java/com/example/demo/SpringBootApplication10Application.java
+++ src/main/java/com/example/demo/SpringBootApplication10Application.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootApplication10Application {
 
-    int y=200;
+	int y = 200;
 
-	
 	int x = 100;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication10Application.class, args);
+	}
+
+	public void m1() {
+		System.out.println("m1--Method added");
 	}
 
 }

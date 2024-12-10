17fbdb6fdbe030bdb6bee89a29bfea1182e7df98
saurabhtipule <tipulesaurabh12@gmail.com>
2024-12-10 13:21:39 +0530

    x var added

--- src/main/java/com/example/demo/SpringBootApplication10Application.java
+++ src/main/java/com/example/demo/SpringBootApplication10Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootApplication10Application {
+	
+	int x = 100;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication10Application.class, args);
